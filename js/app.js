165487765ecf631534e5267759df9d3cee579a27
sklalaludek <pattiegolis@gmail.com>
2017-06-19 22:01:39 +0200

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 $(function() {
-    console.log('working!');
-    
+    var key = 'X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi';
+    // var nasaUrl = 'https://api.nasa.gov/EPIC/api/natural?api_key=X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi ',
+    var nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key=X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi ',
+          marsUrl = ' ';
+    function getPic(){
+        $.ajax({
+            url: nasaUrl,
+            type: 'GET',
+            dataType: 'json'
+        }).done(function(r){
+            console.log(r);
+        }).fail(function(error){
+            console.log(error);
+        })
+    }
+    getPic();
+
+
 });
--- readme.md
+++ readme.md
@@ -1,5 +1,5 @@
 # NASA API Challenge
 
-> Check out the live demo: [NASA API Challenge]()
+> Check out the live demo: [NASA API Challenge](https://sklalaludek.github.io/NASA-API-Challenge/)
 
 ## Web development tools:
