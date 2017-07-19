20482e73e4f7861663f4956aba5de690f39846c0
sklalaludek <pattiegolis@gmail.com>
2017-07-19 21:58:06 +0200

    add random 3 pics

--- js/app.js
+++ js/app.js
@@ -1,25 +1,39 @@
 $(function() {
     // var key = 'X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi';
     // var nasaUrl = 'https://api.nasa.gov/EPIC/api/natural?api_key=X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi ',
-    var nasaUrl = 'https://api.nasa.gov/planetary/apod?api_key=X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi ',
-          marsUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi',
-          welcome = document.getElementById('welcome'),
-          logo = document.getElementById('welcome__logo');
+    var nasaUrl = 'https://api.nasa.gov/planetary/apod',
+          marsUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000',
+          key = '&api_key=X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi',
+          welcome = $('#welcome'),
+          logo = $('#welcome__logo');
 
     function getRandomInt(min, max) {
           return Math.floor(Math.random() * (max - min + 1)) + min;
     }
 
+    /*random date*/
+    var year = getRandomInt(2013, 2016),
+         month = getRandomInt(1, 12),
+         day = getRandomInt(1,28),
+         randomDate = year + '-' + month + '-' + day;
+
     function getPic(){
         $.ajax({
-            url: nasaUrl,
+            url: nasaUrl + '?date=' + randomDate + key,
             type: 'GET',
             dataType: 'json'
         }).done(function(r){
-            console.log(r);
+            showPic(r);
         }).fail(function(error){
             console.log(error);
         })
+    }
+
+    function showPic(data) {
+        var galleryItems = $('.gallery__item');
+
+        console.log('showPic', data);
+        galleryItems.css("background-image", 'url("' + data.url + '")');
     }
 
     function getMarsPic(){
