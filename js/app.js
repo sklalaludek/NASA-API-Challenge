269eb079d3a74086e91a7f2a79241500f1ce80ec
sklalaludek <pattiegolis@gmail.com>
2017-10-15 21:22:14 +0200

    add particles

--- js/app.js
+++ js/app.js
@@ -1,56 +1,126 @@
-$(function() {
-    var nasaUrl = 'https://api.nasa.gov/planetary/apod',
-          marsUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000',
-          key = '&api_key=X0F091rbxFo3yIow6XjUAGwwZG7MZop99Ov1wDhi',
-          welcome = $('#welcome'),
-          logo = $('#welcome__logo');
+/* -----------------------------------------------
+/* How to use? : Check the GitHub README
+/* ----------------------------------------------- */
 
-    function getRandomInt(min, max) {
-          return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-
-    /*random date*/
-    var year = getRandomInt(2013, 2016),
-         month = getRandomInt(1, 12),
-         day = getRandomInt(1,28),
-         randomDate = year + '-' + month + '-' + day;
-
-    function getPic(){
-        $.ajax({
-            url: nasaUrl + '?date=' + randomDate + key,
-            type: 'GET',
-            dataType: 'json'
-        }).done(function(r){
-            showPic(r);
-        }).fail(function(error){
-            console.error(error);
-        })
-    }
-
-    function showPic(data) {
-        var galleryItem = $('.gallery__item');
-
-        console.log('showPic', data);
-        galleryItem.css("background-image", 'url("' + data.url + '")');
-    }
-
-    function getMarsPic(){
-        $.ajax({
-            url: marsUrl,
-            type: 'GET',
-            dataType: 'json'
-        }).done(function(r){
-            console.log(r);
-        }).fail(function(error){
-            console.log(error);
-        })
-    }
-
-    function intro(){
-        $('.animation-fly-in').removeClass('hidden');
-    }
-
-    $(window).on('load', intro());
-    getPic();
-    // getMarsPic();
+/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
+/*
+particlesJS.load('particles-js', 'particles.json', function() {
+  console.log('particles.js loaded - callback');
 });
+*/
+
+/* Otherwise just put the config content (json): */
+
+particlesJS('particles-js',
+    {
+        "particles": {
+        "number": {
+          "value": 160,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "shape": {
+          "type": "circle",
+          "stroke": {
+            "width": 0,
+            "color": "#000000"
+          },
+          "polygon": {
+            "nb_sides": 5
+          },
+          "image": {
+            "src": "img/github.svg",
+            "width": 100,
+            "height": 100
+          }
+        },
+        "opacity": {
+          "value": 1,
+          "random": true,
+          "anim": {
+            "enable": true,
+            "speed": 1,
+            "opacity_min": 0,
+            "sync": false
+          }
+        },
+        "size": {
+          "value": 3,
+          "random": true,
+          "anim": {
+            "enable": false,
+            "speed": 4,
+            "size_min": 0.3,
+            "sync": false
+          }
+        },
+        "line_linked": {
+          "enable": false,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
+        },
+        "move": {
+          "enable": true,
+          "speed": 1,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": false,
+            "rotateX": 600,
+            "rotateY": 600
+          }
+        }
+        },
+        "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+          "onhover": {
+            "enable": true,
+            "mode": "bubble"
+          },
+          "onclick": {
+            "enable": true,
+            "mode": "repulse"
+          },
+          "resize": true
+        },
+        "modes": {
+          "grab": {
+            "distance": 400,
+            "line_linked": {
+              "opacity": 1
+            }
+          },
+          "bubble": {
+            "distance": 250,
+            "size": 0,
+            "duration": 2,
+            "opacity": 0,
+            "speed": 3
+          },
+          "repulse": {
+            "distance": 400,
+            "duration": 0.4
+          },
+          "push": {
+            "particles_nb": 4
+          },
+          "remove": {
+            "particles_nb": 2
+          }
+        }
+        },
+        "retina_detect": true
+    }
+
+);
